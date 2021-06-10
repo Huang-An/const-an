868d93bfe58864fe133c3f739aa9bc864c8fdd34
huangjianbin <hjb@emspost.com.cn>
2021-06-10 14:23:14 +0800

    feat(regexp): 添加websocket链接正则

--- packages/regexp/src/index.ts
+++ packages/regexp/src/index.ts
@@ -19,3 +19,5 @@ export const intranet =
   /^(127\.0\.0\.1)|(localhost)|(10\.\d{1,3}\.\d{1,3}\.\d{1,3})|(172\.((1[6-9])|(2\d)|(3[01]))\.\d{1,3}\.\d{1,3})|(192\.168\.\d{1,3}\.\d{1,3})/
 
 export const space = /^[\s]*$/
+
+export const websocket = /^wss?:/
