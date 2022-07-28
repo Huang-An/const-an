c89097c2babb96dcb0ba9ed648f6bce0889461d2
Huang JianBin <huangjianbin@jms.com>
2022-07-28 12:06:43 +0800

    feat(helper-core/type): 新增 is 类型推断

--- packages/helper-core/src/type.ts
+++ packages/helper-core/src/type.ts
@@ -1,18 +1,18 @@
 const _typeof = (value: any, type: string) => Object.prototype.toString.call(value) === type
 
-export const isNull = (value: any) => _typeof(value, '[object Null]')
+export const isNull = (value: any): value is null => _typeof(value, '[object Null]')
 
-export const isArray = (value: any) => _typeof(value, '[object Array]')
+export const isArray = <T extends [] = []>(value: any): value is T => _typeof(value, '[object Array]')
 
-export const isString = (value: any) => _typeof(value, '[object String]')
+export const isString = (value: any): value is string => _typeof(value, '[object String]')
 
-export const isNumber = (value: any) => _typeof(value, '[object Number]')
+export const isNumber = (value: any): value is number => _typeof(value, '[object Number]')
 
-export const isObject = (value: any) => _typeof(value, '[object Object]')
+export const isObject = <T extends {} = {}>(value: any): value is T => _typeof(value, '[object Object]')
 
-export const isBoolean = (value: any) => _typeof(value, '[object Boolean]')
+export const isBoolean = (value: any): value is boolean => _typeof(value, '[object Boolean]')
 
-export const isUndefined = (value: any) => _typeof(value, '[object Undefined]')
+export const isUndefined = (value: any): value is undefined => _typeof(value, '[object Undefined]')
 
 /**
  * 判断是否为 空字符串、空数组、空对象、"Null"、"null"、"Undefined"、"undefined"、null、undefined
